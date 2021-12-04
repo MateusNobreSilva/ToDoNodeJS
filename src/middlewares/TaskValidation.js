3aac037223a3b91c84cfe5f4809cbc86822bbca4
Mateus_Nobre <mateusnobresilva@gmail.com>
2021-12-04 09:27:17 -0300

    FEATURE ADD - Update, AND - Validation

--- src/middlewares/TaskValidation.js
+++ src/middlewares/TaskValidation.js
@@ -18,6 +18,29 @@ const TaskValidation = async(req, res, next) => {
     } else if (isPast(new Date(when))) {
         return res.status(400).json({ error: 'Escolha uma data e hora futura' });
     } else {
+        let exists;
+
+        if (req.params.id) {
+            exists = await TaskModel.
+            findOne({
+                '_id': { '$ne': req.params.id },
+                'when': { '$eq': new Date(when) },
+                'macaddress': { '$in': macaddress }
+            });
+
+        } else {
+            exists = await TaskModel.
+            findOne({
+                'when': { '$eq': new Date(when) },
+                'macaddress': { '$in': macaddress }
+            });
+        }
+
+
+
+        if (exists) {
+            return res.status(400).json({ error: 'já existe tafrefa nesse dia e horario' });
+        }
         next();
     }
 }
