a751d807695094528d6e0fb2ef89eeda50ace067
Mateus_Nobre <mateusnobresilva@gmail.com>
2021-12-03 21:27:42 -0300

    Comit

--- src/middlewares/TaskValidation.js
+++ src/middlewares/TaskValidation.js
@@ -1,4 +1,5 @@
 const TaskModel = require('../model/TaskModel');
+const { isPast } = require('date-fns');
 
 const TaskValidation = async(req, res, next) => {
 
@@ -6,10 +7,19 @@ const TaskValidation = async(req, res, next) => {
 
     if (!macaddress) {
         return res.status(400).json({ error: 'Macadddres é obrigatório' });
+    } else if (!type) {
+        return res.status(400).json({ error: 'Type é obrigatório' });
+    } else if (!title) {
+        return res.status(400).json({ error: 'titulo é obrigatório' });
+    } else if (!description) {
+        return res.status(400).json({ error: 'description é obrigatório' });
+    } else if (!when) {
+        return res.status(400).json({ error: 'Data e Hora são obrigatório' });
+    } else if (isPast(new Date(when))) {
+        return res.status(400).json({ error: 'Escolha uma data e hora futura' });
     } else {
         next();
     }
-
 }
 
 module.exports = TaskValidation;
