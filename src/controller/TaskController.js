d37e3c5edf189380978a4d786dec4e4530a53d0d
Mateus_Nobre <mateusnobresilva@gmail.com>
2021-12-10 19:32:53 -0300

    FEATURE ADD - Filtro tarefas do dia

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,4 +1,5 @@
 const TaskModel = require('../model/TaskModel');
+const { startOfDay, endOfDay } = require('date-fns');
 
 const current = new Date();
 
@@ -90,6 +91,21 @@ class TaskController {
             .catch(error => {
                 return res.status(500).json(error);
             });
+    }
+
+    async today(req, res) {
+        await TaskModel
+            .find({
+                'macaddress': { '$in': req.body.macaddress },
+                'when': {'$gte': startOfDay(current), '$lte': endOfDay(current) }
+            })
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error =>{
+                return res.status(500).json(error);
+            })
     }
 
 
